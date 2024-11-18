035558206ea9c9ef5486dbcb524a6390fb140df5
Gu Xi <zjuguxi@gmail.com>
2024-11-17 20:48:32 -0800

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const [aiData, setAiData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8080')
+    axios.get(process.env.API_URL)
       .then(response => {
         const formattedData = response.data.map(item => ({
           date: new Date(item.date).toLocaleDateString(),
